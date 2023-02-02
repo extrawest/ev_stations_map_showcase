4abaaee02bbc28032acae3874a75787fc66f5de5
Ruslan Volkhov <ruslan.volkhov@extrawest.com>
2023-02-03 00:35:22 +0200

    added map screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:volkhov_maps_app/screens/map_sreen.dart';
 
 import 'flavor_banner.dart';
 import 'screens/home_screen.dart';
@@ -6,8 +7,10 @@ import 'screens/splash_screen.dart';
 
 const String homeScreenRoute = '/home_screen';
 const String splashScreenRoute = '/splash_screen';
+const String mapScreenRoute = '/map_screen';
 
 Map<String, WidgetBuilder> applicationRoutes = <String, WidgetBuilder>{
   splashScreenRoute: (context) => SplashScreen(),
   homeScreenRoute: (context) => const FlavorBanner(child: HomeScreen()),
+  mapScreenRoute: (context) => const MapScreen(),
 };
