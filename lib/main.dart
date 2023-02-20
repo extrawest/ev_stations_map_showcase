68e74c38fc6df1a46fe541456b7ca2756435e2c6
Ruslan Volkhov <ruslan.volkhov@extrawest.com>
2023-02-21 00:59:29 +0200

    added firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:volkhov_maps_app/app.dart';
 import 'package:volkhov_maps_app/simple_bloc_observer.dart';
 import 'package:volkhov_maps_app/theme/assets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+import 'firebase_options.dart';
 import 'utils/application_utils.dart';
 import 'utils/logger.dart';
 
@@ -24,6 +27,9 @@ bool isProduction = false;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   Bloc.observer = SimpleBlocObserver();
 
