ea8614f63abe3e23c310a59d9e9e14cb5d8d194c
Ruslan Volkhov <ruslan.volkhov@extrawest.com>
2023-02-27 10:43:00 +0200

    setting for codemagic

--- android/app/src/main/java/com/extrawest/volkhov_maps_app/MainActivity.kt
+++ android/app/src/main/java/com/extrawest/volkhov_maps_app/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.extrawest.volkhov_maps_app
 
-import androidx.annotation.NonNull;
+import android.content.Context
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin
 
-class MainActivity : FlutterActivity() {
+class MainActivity: FlutterActivity() {
+override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    flutterEngine.getPlugins().add(SharedPreferencesPlugin())
+}
 }
